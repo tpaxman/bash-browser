26dd15bdf25fc5d6b2e2037b91588ebc16927718
Tyler Paxman <tylerpaxman@gmail.com>
2020-10-24 15:26:15 -0600

    Revert "add google as default page when script is called without arguments"
    This reverts commit 2a9d94895568600c7ecce755bf14f76a5627a330.

--- browse.sh
+++ browse.sh
@@ -1,58 +1,54 @@
 #!/bin/bash
 
-if [ "$#" -gt "0" ]; then
-    # look up website based on keyword and guess otherwise
-    case $1 in
-        github | github.com | git)
-            url="www.github.com"
-            query="search?q="
-            ;;
-        google | google.com | google.ca)
-            url="www.google.com"
-            query="search?q="
-            ;;
-        whatsapp)
-            url="web.whatsapp.com"
-            ;;
-        dyna | dynalist | dynalist.io)
-            url="www.dynalist.io"
-            query="#q="
-            ;;
-        youtube | you | youtube.com)
-            url="www.youtube.com"
-            query="results?search_query="
-            ;;
-        reddit | red | reddit.com)
-            url="www.reddit.com"
-            query="search?q="
-            ;;
-        gmail | gmail.com | mail.google.com | mail)
-            url="mail.google.com/mail/u/0"
-            query="#search/"
-            ;;
-        stackoverflow | stack | so | stackoverflow.com)
-            url="www.stackoverflow.com"
-            query="search?q="
-            ;;
-        imdb | imdb.com)
-            url="www.imdb.com"
-            query="find?q="
-            ;;
-        *)
-            url=$1
-            query="search?q="
-    esac
-else
-    url='www.google.com'
-fi
+# look up website based on keyword and guess otherwise
+case $1 in
+    github | github.com | git)
+        url="www.github.com"
+        query="search?q="
+        ;;
+    google | google.com | google.ca)
+        url="www.google.com"
+        query="search?q="
+        ;;
+    whatsapp)
+        url="web.whatsapp.com"
+        ;;
+    dyna | dynalist | dynalist.io)
+        url="www.dynalist.io"
+        query="#q="
+        ;;
+    youtube | you | youtube.com)
+        url="www.youtube.com"
+        query="results?search_query="
+        ;;
+    reddit | red | reddit.com)
+        url="www.reddit.com"
+        query="search?q="
+        ;;
+    gmail | gmail.com | mail.google.com | mail)
+        url="mail.google.com/mail/u/0"
+        query="#search/"
+        ;;
+    stackoverflow | stack | so | stackoverflow.com)
+        url="www.stackoverflow.com"
+        query="search?q="
+        ;;
+    imdb | imdb.com)
+        url="www.imdb.com"
+        query="find?q="
+        ;;
+    *)
+        url=$1
+        query="search?q="
+esac
 
 # assume all the other arguments are search terms to look up on the site
 if [ "$#" -gt "1" ]; then
     searchterm=$2
-    for term in ${@: 3}; do
-         searchterm+="%20$term"
-    done
-    url+="/$query$searchterm"
+	for term in ${@: 3}; do
+	     searchterm+="%20$term"
+	done
+	url+="/$query$searchterm"
 fi
 
 # open the default browser and navigate to the page
